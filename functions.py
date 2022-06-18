6b920b46f0679c8acbaf067f567113dfad6e3631
Juan Pablo <if721093@iteso.mx>
2022-06-17 23:53:16 -0500

    martingale function mid per minute

--- functions.py
+++ functions.py
@@ -14,6 +14,7 @@ import numpy as np
 import data as dt
 import pandas as pd
 from scipy.stats import skew,kurtosis
+import itertools
 
 
 # imported data
@@ -139,3 +140,224 @@ def f_descriptive_ob(data_ob:dict) -> dict:
     
     
     return r_data
+
+
+def f_martingale(data_ob:dict,price:str='midprice',interval:str='None') -> dict:
+    """
+    
+
+    Parameters
+    ----------
+    data_ob : dict
+        
+        Orderbook as the input data, a dictionary with the following structure:
+        "timestamp": object timestamp recognize by machine, e.g. pd.to_datetime()
+        'bid_size:'volume for bid levels
+        'bid:'bid price
+        'ask:'ask price
+        'ask_size: volume of ask levels
+    
+    price : str
+         Define the type of price you want to test for your analysis pourpuse.
+         By default it is set to work with the midprice, but it can also work
+         weighted_midprice. 
+         
+         For the weighted midprice: the argument is 'weighted_midprice'
+        For the midprice is: 'midprice'
+        If the argument is not specify it would be calculated with the 
+        midprice by default.
+         
+    interval : str
+          The martingale property can be test with all the prices in the 
+          Order Book or it can be done by minute interval for the first hour.
+          For the interval in minutes the argument should be 'minutes'.
+          If the argument is not given by default it would be calculated 
+          with all the prices of the order book.
+
+
+    Returns
+    -------
+    dict
+        When the calculation is done without the minute interval, the function
+        returns a dictionary with the following keys:
+        
+            e1: a dictionary with the number of ocurrencies where the 
+                first scenario (e1) happen, and the proportion respect 
+                to the total experiemnt trials.
+                e1 = el w_midprice_t = w_midprice_t+1 
+                
+             e2: a dictionary with the number of ocurrencies where the 
+                first scenario (e2) happen, and the proportion respect 
+                to the total experiemnt trials.
+                e2 = w_midprice_t != w_midprice_t+1
+                
+    DataFrame
+        When the calculation is done by minute interval, the function returns
+        a pandas dataframe compound of the following columns:
+            
+            interval: The minute 
+            
+            total: The sum of the experiments for each minute.
+            
+            e1: Number of trials that fullfill the first scenario.
+            
+            e2: Number of trials that fullfill the second scenario.
+            
+            proportion e1: The proportion of the first scenario that fullfill
+            the first scenario respect to the total trails for each minute.
+            
+            proportion e2: The proportion of the second scenario that fullfill
+            the first scenario respect to the total trails for each minute.
+            
+
+    """
+
+       
+   
+    if price == 'midprice' and interval=='minutes':
+        # only for the first hour
+        # keys en timestamp
+        #l_ts =[pd.to_datetime(i) for i in  list(data_ob.keys())][9:] 
+        minutes = list(np.arange(0,60)) #indexer
+       
+       #search for each minutes the orderbooks and calculate mid
+
+        #getting the keys 
+        ts = dict(itertools.islice(data_ob.items(),2401-9))
+        ts = list(ts.keys())
+        # calculating the mid as before
+        #mid2 = [(dt.ob_data[i]['ask'][0]+dt.ob_data[i]['bid'][0])*0.5 for i in ts.keys()]
+        mid2 = [(data_ob[ts[i]]['ask'][0]+data_ob[ts[i]]['bid'][0])*0.5
+                for i in range(0,len(ts))]
+        
+        #creating an empty dataframe to save mid and timestamp
+        df = pd.DataFrame()
+        df['mid']=mid2
+        df['time']=[pd.to_datetime(i) for i in ts]
+        df['minute']=[i.minute for i in df['time'].tolist()]
+        
+        
+        #grouping in a dictionary the mids that happen in each minute
+        d = dict((i,list(df[df['minute']==i]['mid'])) for i in minutes)
+        
+        #saving the experiment's results  (proportions, counts) in a dataframe
+        e1_m = []
+        e2_m = []
+        prop_e1 = []
+        prop_e2 = []
+        
+        for i in range(len(d)):
+            e1_t = np.sum([d[i][j] == d[i][j+1] for j in range(0,len(d[i])-1)])
+            e1_m.append(e1_t)
+            e2_t=len(d[i])-1 - np.sum([d[i][j] == d[i][j+1] for j in range(0,len(d[i])-1)])
+            e2_m.append(e2_t)
+            total2 = len(d[i])-1
+            prop_e1.append(np.round(e1_t/total2,2))
+            prop_e2.append(np.round(e2_t/total2,2))
+            
+        exp_2 = pd.DataFrame()
+        exp_2['interval']=minutes
+        exp_2['total']=[i+j for i,j in zip(e1_m,e2_m)]
+        exp_2['e1']=e1_m
+        exp_2['e2']=e2_m
+        exp_2['proportion e1'] = prop_e1
+        exp_2['proportion e2'] = prop_e2
+        
+        return exp_2
+    
+    if price=='weighted_midprice' and interval=='minutes':
+        
+        minutes = list(np.arange(0,60)) #indexer
+        ts = dict(itertools.islice(dt.ob_data.items(),2401-9))
+
+        # calculating the mid as before
+        w_mid = [data_ob[i]['bid_size'].sum()/(data_ob[i]['bid_size'].sum()+data_ob[i]['ask_size'].sum())*(data_ob[i]['ask'][0]+data_ob[i]['bid'][0])*0.5 
+                  for i in ts.keys()]
+        
+        #creating an empty dataframe to save mid and timestamp
+        df = pd.DataFrame()
+        df['weighted_mid']=w_mid
+        df['time']=[pd.to_datetime(i) for i in ts]
+        df['minute']=[i.minute for i in df['time'].tolist()]
+
+
+        #grouping in a dictionary the mids that happen in each minute
+        d = dict((i,list(df[df['minute']==i]['weighted_mid'])) for i in minutes)
+        
+        #saving the experiment's results  (proportions, counts) in a dataframe
+
+        e1 = []
+        e2 = []
+        prop_e1 = []
+        prop_e2 = []
+        
+        for i in range(len(d)):
+            e1_t = np.sum([d[i][j] == d[i][j+1] for j in range(len(d[i])-1)])
+            e1.append(e1_t)
+            e2_t=len(d[i])-1 - np.sum([d[i][j] == d[i][j+1] for j in range(len(d[i])-1)])
+            e2.append(e2_t)
+            total = len(d[i])-1
+            prop_e1.append(np.round(e1_t/total,2))
+            prop_e2.append(np.round(e2_t/total,2))
+            
+        exp_2 = pd.DataFrame()
+        exp_2['interval']=minutes
+        exp_2['total']=[i+j for i,j in zip(e1,e2)]
+        exp_2['e1']=e1
+        exp_2['e2']=e2
+        exp_2['proportion e1'] = prop_e1
+        exp_2['proportion e2'] = prop_e2
+        
+        return exp_2
+    
+    if price=='weighted_midprice' and interval=='None':
+        
+        ts = dict(itertools.islice(data_ob.items(),2401-9))
+
+# calculating the mid as before
+        w_mid = [data_ob[i]['bid_size'].sum()/(data_ob[i]['bid_size'].sum()+data_ob[i]['ask_size'].sum())*(data_ob[i]['ask'][0]+data_ob[i]['bid'][0])*0.5 
+          for i in ts.keys()]
+        
+        
+        #count ocurrencies for each scenario
+        
+        # e1 = el w_midprice_t = w_midprice_t+1 
+        # e2 = w_midprice_t != w_midprice_t+1
+        
+        # formula P_t = E{P_t+1}
+        total = len(w_mid)-1
+        e1_w =[ w_mid[i] == w_mid[i+1] for i in range(len(w_mid)-1)]
+        e2_w = len(w_mid)-1 - sum(e1_w)
+        
+        #save the results, counts and  proportions in a dictionary
+        
+        exp_1 = {'e1':{'cantidad':sum(e1_w), 'proporcion':np.round(sum(e1_w)/total,2)},
+                    'e2':{'cantidad':e2_w, 'proporcion': np.round(e2_w/total,2)},
+                    'total':len(w_mid)-1 }
+        
+        return exp_1
+        
+    
+    else:
+       ob_ts = list(data_ob.keys())
+       #mid caluclation
+       mid =  [(data_ob[ob_ts[i]]['ask'][0]+data_ob[ob_ts[i]]['bid'][0])*0.5 
+               for i in range(0,len(ob_ts))]
+       
+       #martingale test
+       total = len(mid)-1
+       e1 =[ mid[i] == mid[i+1] for i in range(len(mid)-1)]
+       e2 = len(mid)-1 - sum(e1)
+       
+       exp_1 = {'e1':{'cantidad':sum(e1), 'proporcion':np.round(sum(e1)/total,2)},
+            'e2':{'cantidad':e2, 'proporcion': np.round(e2/total,2)},
+            'total':len(mid)-1 }
+       
+       return exp_1
+       
+       
+          
+           
+           
+        
+       
